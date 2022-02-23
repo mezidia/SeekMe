4151a22fdc8f4b0eca9f1784cf9edbd3cfd83d36
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-02-23 12:37:01 +0200

    Rewrite urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.get_routes, name='routes'),
-    path('orders/', views.OrderList.as_view()),
-    path('orders/<int:pk>/', views.OrderDetail.as_view()),
+    path('orders/', views.order_list, name='orders'),
+    path('order/<int:pk>', views.order_detail, name='order'),
+    path('flights/', views.flight_list, name='flights'),
+    path('flight/<int:pk>', views.flight_detail, name='flight'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import mixins, generics
-
-from .serializers import OrderSerializer, FlightSerializer
 
 
 @api_view(['GET'])
