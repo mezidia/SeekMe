58821693cc0d09c18bc9bfe38a812809cc193776
Dmytrenko-Roman <savitarreus@gmail.com>
2022-04-12 17:11:25 +0300

    ❄️ User model added ❄️

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,11 +1,27 @@
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 from database import Base
 
 
 class PostModel(Base):
-    __tablename__ = "post"
+    __tablename__ = "posts"
 
-    id = Column(Integer, Sequence("seq_street_segment_id"))
+    id = Column(Integer, primary_key=True, nullable=False, index=True)
     name = Column(String, primary_key=True, nullable=False)
     description = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    creator = relationship("User", back_populates="heroes")
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    phone_number = Column(String, nullable=False)
+
+    posts = relationship("Hero", back_populates="creator")
