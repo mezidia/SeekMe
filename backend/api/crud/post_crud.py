4a2ba357fa1c578db31c7933de50f8fa32d6bfc9
Dmytrenko-Roman <savitarreus@gmail.com>
2022-04-13 10:13:55 +0300

    ❄️ Added delete operation ❄️

--- backend/api/crud/post_crud.py
+++ backend/api/crud/post_crud.py
@@ -16,3 +16,22 @@ def create_post(user_id: int, post: schemas.PostCreate, db: Session):
     db.refresh(db_post)
 
     return db_post
+
+# def update_user(id: id, user: schemas.UserBase, db: Session) -> dict:
+#     db_user = db.query(models.User).filter(models.User.id == id)
+
+#     db_user.update(
+#         {
+#             "name": user.name,
+#             "email": user.email,
+#             "phone_number": user.phone_number,
+#         },
+#         synchronize_session=False,
+#     )
+#     db.commit()
+
+def delete_post(id: id, db: Session):
+    db_post = db.query(models.Post).filter(models.Post.id == id)
+
+    db_post.delete(synchronize_session=False)
+    db.commit()
