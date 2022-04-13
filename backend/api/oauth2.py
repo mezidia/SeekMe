170cc26bacae9e43567071d7b627005ff3847ce7
Dmytrenko-Roman <savitarreus@gmail.com>
2022-04-13 18:33:19 +0300

    ❄️ Login is working ❄️

--- backend/api/oauth2.py
+++ backend/api/oauth2.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 
 from config import settings
 from crud import user_crud
-import schemas, database, models
+import schemas, database
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -24,19 +24,20 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    print(encoded_jwt)
 
     return encoded_jwt
 
 
 def verify_access_token(token: str, credentials_exception):
     try:
-        print(token)
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id = payload.get("sub")
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
-    except JWTError:
+    except JWTError as error:
+        print(error)
         raise credentials_exception
 
     return token_data
@@ -55,3 +56,5 @@ def get_current_user(
     user = user_crud.get_user_by_id(token.id, db)
 
     return user
+
+
--- backend/api/routers/auth.py
+++ backend/api/routers/auth.py
@@ -22,5 +22,5 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token = create_access_token(data={"sub": user.id})
+    access_token = create_access_token(data={"sub": str(user.id)})
     return {"access_token": access_token, "token_type": "bearer"}
