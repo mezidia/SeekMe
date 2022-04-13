1789b0cf8f20c967011c34688a5818704d94fa1c
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-13 16:22:58 +0300

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
-      <p>Hello</p>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.id}. {post.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await data.json();
+  return {
+    props: {
+      posts,
+    },
+  };
 }
