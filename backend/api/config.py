91c53e1ab19131914a43772cedc894fdbde9ce50
Dmytrenko-Roman <savitarreus@gmail.com>
2022-04-13 16:54:36 +0300

    ❄️ Schemas for token ❄️

--- backend/api/config.py
+++ backend/api/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     # database_username: str
     # database_password: str
     # database_name: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
 
     class Config:
         env_file = "../.env"
--- backend/api/schemas.py
+++ backend/api/schemas.py
@@ -36,3 +36,23 @@ class User(UserBase):
 
     class Config:
         orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
