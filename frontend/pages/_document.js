9e53ec2d83fd908f7cca9a4481f44b5628783342
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-13 09:52:15 +0300

    Update _document.js
    Add Bootstrap js bundle

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
         />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
       </Head>
       <body>
         <Main />
