4ed0ac5f519b0d8fd961b541f3fab8461c688ea5
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-04-14 10:44:30 +0300

    ♻️: Ya krasavec`

--- backend/api/routers/post.py
+++ backend/api/routers/post.py
@@ -28,41 +28,37 @@ def get_posts(db: Session = Depends(get_db)):
     return posts
 
 
-@router.post("/create/", status_code=201, response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = post_crud.get_user_by_email(user.email, db)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-
-    return post_crud.create_user(user, db)
+@router.post("/create/", status_code=201, response_model=schemas.Post)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
+    return post_crud.create_post(current_user.id, post, db)
 
 
-@router.put("/update/{id}", status_code=200)
-def update_user(
-    id: int,
-    user: schemas.UserBase,
-    db: Session = Depends(get_db),
-    current_user: schemas.User = Depends(get_current_user),
-):
-    if current_user.id != id:
-        raise HTTPException(
-            status_code=403, detail=f"This operation is not allowed without log in"
-        )
-    post_crud.update_user(id, user, db)
+# @router.put("/update/{id}", status_code=200)
+# def update_user(
+#     id: int,
+#     user: schemas.UserBase,
+#     db: Session = Depends(get_db),
+#     current_user: schemas.User = Depends(get_current_user),
+# ):
+#     if current_user.id != id:
+#         raise HTTPException(
+#             status_code=403, detail=f"This operation is not allowed without log in"
+#         )
+#     post_crud.update_user(id, user, db)
 
-    return {"detail": f"user with id {id} was updated"}
+#     return {"detail": f"user with id {id} was updated"}
 
 
-@router.delete("/delete/{id}", status_code=404)
-def delete_user(
-    id: int,
-    db: Session = Depends(get_db),
-    current_user: schemas.User = Depends(get_current_user),
-):
-    if current_user.id != id:
-        raise HTTPException(
-            status_code=403, detail=f"This operation is not allowed without log in"
-        )
-    post_crud.delete_user(id, db)
+# @router.delete("/delete/{id}", status_code=404)
+# def delete_user(
+#     id: int,
+#     db: Session = Depends(get_db),
+#     current_user: schemas.User = Depends(get_current_user),
+# ):
+#     if current_user.id != id:
+#         raise HTTPException(
+#             status_code=403, detail=f"This operation is not allowed without log in"
+#         )
+#     post_crud.delete_user(id, db)
 
-    return Response(status_code=404)
+#     return Response(status_code=404)
