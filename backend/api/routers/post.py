b3f6e628778c9717d73915b884116aa847a4e53e
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-04-14 10:35:28 +0300

    ♻️: update crud

--- backend/api/routers/post.py
+++ backend/api/routers/post.py
@@ -9,14 +9,9 @@ import schemas
 router = APIRouter(prefix="/posts", tags=["posts"])
 
 
-@router.get("/me", response_model=schemas.User)
-async def read_users_me(current_user: schemas.User = Depends(get_current_user)):
-    return current_user
-
-
 @router.get("/{id}", status_code=200, response_model=schemas.User)
 def get_user(id: int, db: Session = Depends(get_db)):
-    user = user_crud.get_user_by_id(id, db)
+    user = post_crud.get_user_by_id(id, db)
     if user is None:
         raise HTTPException(
             status_code=404,
@@ -28,18 +23,18 @@ def get_user(id: int, db: Session = Depends(get_db)):
 
 @router.get("/", status_code=200, response_model=list[schemas.User])
 def get_users(db: Session = Depends(get_db)):
-    users = user_crud.get_all_users(db)
+    users = post_crud.get_all_users(db)
 
     return users
 
 
 @router.post("/create/", status_code=201, response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = user_crud.get_user_by_email(user.email, db)
+    db_user = post_crud.get_user_by_email(user.email, db)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    return user_crud.create_user(user, db)
+    return post_crud.create_user(user, db)
 
 
 @router.put("/update/{id}", status_code=200)
@@ -53,7 +48,7 @@ def update_user(
         raise HTTPException(
             status_code=403, detail=f"This operation is not allowed without log in"
         )
-    user_crud.update_user(id, user, db)
+    post_crud.update_user(id, user, db)
 
     return {"detail": f"user with id {id} was updated"}
 
@@ -68,6 +63,6 @@ def delete_user(
         raise HTTPException(
             status_code=403, detail=f"This operation is not allowed without log in"
         )
-    user_crud.delete_user(id, db)
+    post_crud.delete_user(id, db)
 
     return Response(status_code=404)
