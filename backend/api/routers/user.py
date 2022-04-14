7ed9eb15074274eecae09648725ce35c7a0414cb
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-04-14 13:43:29 +0300

    📝: add documentation for read_users_me

--- backend/api/routers/user.py
+++ backend/api/routers/user.py
@@ -11,6 +11,12 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 @router.get("/me", response_model=schemas.User)
 async def read_users_me(current_user: schemas.User = Depends(get_current_user)):
+    """
+    read_users_me reads current user.
+
+    :param current_user: current user
+    :return: current user.
+    """
     return current_user
 
 
