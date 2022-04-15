59fd6b68e06bf8fc3489f9071c204b4506cb7491
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-15 16:04:22 +0300

    Update layout.js

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -7,13 +7,13 @@ export default function Layout({ children }) {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="title" content="" />
+        <meta name="title" content="ЗнайдиМене" />
         <meta name="description" content="" />
         <meta name="keywords" content="" />
         <meta name="robots" content="index, follow" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
-        <title>SeekMe</title>
+        <title>ЗнайдиМене</title>
       </Head>
       <Navbar />
       <div className="container-fluid">{children}</div>
