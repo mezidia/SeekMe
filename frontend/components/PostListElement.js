a95651a34781abd45062cdf260d646168b7b9088
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-15 21:04:45 +0300

    Update PostListElement.js

--- frontend/components/PostListElement.js
+++ frontend/components/PostListElement.js
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function PostListElement({ post }) {
   return (
     <li>
-      {post.description}.{" "}
+      <Link href={`/post/${post.id}`}>
+        <a>{post.full_name}</a>
+      </Link>
+      .{" "}
       <Link href={`/user/${post.owner_id}`}>
         <a>Author</a>
       </Link>
