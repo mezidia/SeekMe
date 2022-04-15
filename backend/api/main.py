780c23561db36c693f420a0ef701e0116d0d667a
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-15 08:49:52 +0300

    Update main.py
    Add middleware for CORS

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine
 from routers import user, post, auth
@@ -6,6 +7,19 @@ import models
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
