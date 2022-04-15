e4a7e81694635e992c03f9d2a317525f6519404e
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-15 16:16:56 +0300

    Update UserHeader.js
    Show user's name

--- frontend/components/UserHeader.js
+++ frontend/components/UserHeader.js
@@ -12,10 +12,25 @@ export default function UserHeader({ setToken }) {
   };
 
   const fetchUser = async () => {
-    const data = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await data.json();
-    const user = users[Math.floor(Math.random() * users.length)];
-    setUserName(user.name);
+    const token = localStorage.getItem("token");
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    };
+    const response = await fetch(
+      "http://127.0.0.1:8000/users/me",
+      requestOptions
+    );
+    if (!response.ok) {
+      localStorage.removeItem("token");
+    } else {
+      const user = await response.json();
+      setUserName(user.name);
+    }
   };
 
   useEffect(() => {
