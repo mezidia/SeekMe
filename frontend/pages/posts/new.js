dfdeb5b687d31bffb69b8f7257dd8743578dc08a
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-16 18:41:44 +0300

    Create function

--- frontend/pages/posts/new.js
+++ frontend/pages/posts/new.js
@@ -1,9 +1,12 @@
 import { useRef, useState, useEffect } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 import checkUser from "../../utils/checkUser";
 import Error from "../../components/Error";
 
 export default function NewPost() {
+  const router = useRouter();
   const nameRef = useRef();
   const descriptionRef = useRef();
   const placeRef = useRef();
@@ -11,6 +14,58 @@ export default function NewPost() {
 
   const [error, setError] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const createPost = async (data) => {
+    const name = nameRef.current.value;
+    const description = descriptionRef.current.value;
+    const place = placeRef.current.value;
+    const token = localStorage.getItem("token");
+    const response = await fetch("http://127.0.0.1:8000/posts/create/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+        full_name: name,
+        description,
+        last_place: place,
+        image: data.file_path,
+      }),
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      setError(data.detail);
+    } else {
+      router.push(`/posts${data.id}`);
+    }
+  };
+
+  const postImage = async () => {
+    const formData = new FormData();
+    const image = imageRef.current.files[0];
+    formData.append("file", image);
+
+    const response = await fetch("http://127.0.0.1:8000/posts/add/image/", {
+      method: "POST",
+      body: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Accept: "application/json",
+      },
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      setError(data.detail);
+    } else {
+      createPost(data);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    postImage();
+  };
 
   useEffect(() => {
     if (checkUser()) {
@@ -20,10 +75,14 @@ export default function NewPost() {
 
   return (
     <>
+      <Head>
+        <title>Створити пост</title>
+      </Head>
+      {error ? <Error error={error} /> : null}
       {!isAuthenticated ? (
         <Error error={"You need to be authenticated"} />
       ) : (
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label for="exampleInputName" className="form-label">
               Повне ім'я
