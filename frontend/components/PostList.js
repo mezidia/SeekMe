95c2fe87ac0197b4791725ea9ad5aa1c9664ab4d
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-18 20:38:43 +0300

    Update post list element

--- frontend/components/PostList.js
+++ frontend/components/PostList.js
@@ -2,12 +2,10 @@ import PostListElement from "./PostListElement";
 
 export default function PostList({ posts }) {
   return (
-    <div>
-      <ul>
-        {posts.map((post) => (
-          <PostListElement post={post} key={post.id} />
-        ))}
-      </ul>
+    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 pt-2">
+      {posts.map((post) => (
+        <PostListElement post={post} key={post.id} />
+      ))}
     </div>
   );
 }
--- frontend/components/PostListElement.js
+++ frontend/components/PostListElement.js
@@ -2,14 +2,48 @@ import Link from "next/link";
 
 export default function PostListElement({ post }) {
   return (
-    <li>
-      <Link href={`/posts/${post.id}`}>
-        <a>{post.full_name}</a>
-      </Link>
-      .{" "}
-      <Link href={`/user/${post.owner_id}`}>
-        <a>Author</a>
-      </Link>
-    </li>
+    <div className="col">
+      <div className="card shadow-sm">
+        <svg
+          className="bd-placeholder-img card-img-top"
+          width="100%"
+          height="225"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          aria-label="Placeholder: Thumbnail"
+          preserveAspectRatio="xMidYMid slice"
+          focusable="false"
+        >
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#55595c"></rect>
+          <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+            Thumbnail
+          </text>
+        </svg>
+
+        <div className="card-body">
+          <p className="card-text">{post.description}</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <Link href="/posts/[id]" as={`/posts/${post.id}`}>
+                <a type="button" role="button" className="btn btn-warning">
+                  Почитати більше
+                </a>
+              </Link>
+              <Link href="/user/[id]/" as={`/user/${post.owner_id}`}>
+                <a
+                  type="button"
+                  role="button"
+                  class="btn btn-outline-dark ms-1"
+                >
+                  Інші пости автора
+                </a>
+              </Link>
+            </div>
+            <small>{post.last_place}</small>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
