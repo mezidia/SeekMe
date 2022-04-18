95342bbd54e7d5c2f115b5e05c818fafbad573a0
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-18 20:14:07 +0300

    Update MainPage.js

--- frontend/components/MainPage.js
+++ frontend/components/MainPage.js
@@ -1,21 +1,25 @@
+import Link from "next/link";
+
 export default function MainPage() {
   return (
     <section className="py-5 text-center container">
       <div className="row py-lg-5">
         <div className="col-lg-6 col-md-8 mx-auto">
-          <h1 className="fw-light">Album example</h1>
+          <h1 className="fw-light">ЗнайдиМене</h1>
           <p className="lead text-muted">
             Something short and leading about the collection below—its contents,
             the creator, etc. Make it short and sweet, but not too short so
             folks don’t simply skip over it entirely.
           </p>
           <p>
-            <a href="#" className="btn btn-primary my-2">
-              Main call to action
-            </a>
-            <a href="#" className="btn btn-secondary my-2">
-              Secondary action
-            </a>
+            <Link href="/register">
+              <a className="btn btn-primary btn-lg my-2 me-2">
+                Зареєструватись
+              </a>
+            </Link>
+            <Link href="/login">
+              <a className="btn btn-secondary btn-lg my-2">Увійти</a>
+            </Link>
           </p>
         </div>
       </div>
