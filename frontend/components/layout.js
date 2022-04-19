a271674cd8252405562e131a418bbd970a1d3923
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-19 21:33:31 +0300

    Update layout.js

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -6,13 +6,25 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="title" content="ЗнайдиМене" />
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <meta name="robots" content="index, follow" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="English" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+        <meta name="title" content="ЗнайдиМене" key="title" />
+        <meta
+          name="description"
+          content="Онлайн-сервіс, який допоможе Вам знайти своїх друзів та рідних."
+          key="description"
+        />
+        <meta
+          name="keywords"
+          content="знайти рідних, пошук рідних, пошук друзів, оголошення про родичів, оголошення про зниклих"
+          key="keywords"
+        />
+        <meta name="robots" content="index, follow" key="robots" />
+        <meta charset="UTF-8" key="charset" />
+        <meta name="language" content="Ukrainian" key="language" />
         <title>ЗнайдиМене</title>
       </Head>
       <Navbar />
