4633a5d3547e5b6c05576838d823b770af592b74
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-24 22:02:27 +0300

    Implement pagination

--- frontend/components/PostList.js
+++ frontend/components/PostList.js
@@ -1,11 +1,61 @@
+import { useEffect, useState } from "react";
+
 import PostListElement from "./PostListElement";
 
 export default function PostList({ posts }) {
+  const [paginatedPosts, setPaginatedPosts] = useState(posts);
+  const [page, setPage] = useState(1);
+
+  const postsPerPage = 10;
+
+  const handlePageChange = (pageNumber) => {
+    setPage(pageNumber);
+  };
+
+  useEffect(() => {
+    const handlePagination = () => {
+      const indexOfLastPost = page * postsPerPage;
+      const indexOfFirstPost = indexOfLastPost - postsPerPage;
+      const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+      setPaginatedPosts(currentPosts);
+    };
+    handlePagination();
+  }, [page]);
+
   return (
-    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 pt-2">
-      {posts.map((post) => (
-        <PostListElement post={post} key={post.id} />
-      ))}
-    </div>
+    <>
+      <p className="pt-3 fw-bold">
+        <span>
+          Усього відображено{" "}
+          {postsPerPage * page <= posts.length
+            ? postsPerPage * page
+            : posts.length}
+          {" об'яв(а)"} з {posts.length}
+        </span>
+        <span className="ps-4">
+          {page > 1 ? (
+            <button
+              className="btn btn-primary me-2"
+              onClick={() => handlePageChange(page - 1)}
+            >
+              &larr; Попередня сторінка
+            </button>
+          ) : null}
+          {postsPerPage * page <= posts.length ? (
+            <button
+              className="btn btn-primary"
+              onClick={() => handlePageChange(page + 1)}
+            >
+              Наступна сторінка &rarr;
+            </button>
+          ) : null}
+        </span>
+      </p>
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 pt-2">
+        {paginatedPosts.map((post) => (
+          <PostListElement post={post} key={post.id} />
+        ))}
+      </div>
+    </>
   );
 }
