932a36e7b760838b09102393018f1dc2f0e4047e
Dmytrenko-Roman <savitarreus@gmail.com>
2022-06-06 15:22:05 +0300

    ❄️ Gitignore update ❄️

--- backend/api/database.py
+++ backend/api/database.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # from config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///../sql_app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
