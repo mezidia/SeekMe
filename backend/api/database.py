30afcdf19d09cf6413e79ae995442449215704db
Dmytrenko-Roman <savitarreus@gmail.com>
2022-06-10 17:02:29 +0300

    ❄️ Test for user's /update route ❄️

--- backend/api/database.py
+++ backend/api/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # from config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///../sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///sql_app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
--- backend/api/tests/test_user.py
+++ backend/api/tests/test_user.py
@@ -7,8 +7,16 @@ from api.config import settings
 from .token import access_token
 
 
+user_for_tests = {
+    "name": "something",
+    "email": "something@gmail.com",
+    "phone_number": "1111111",
+    "password": "something",
+}
+
+
 @pytest.mark.anyio
-async def test_me_route():
+async def test_get_route():
     async with AsyncClient(app=app, base_url=settings.alternative_host_for_api) as ac:
         response = await ac.get(
             "/users/me/", headers={"Authorization": f"Bearer {access_token}"}
@@ -18,7 +26,25 @@ async def test_me_route():
 
 
 @pytest.mark.anyio
-async def test_users_route():
+async def test_get_by_id_route():
+    async with AsyncClient(app=app, base_url=settings.alternative_host_for_api) as ac:
+        id = 1
+        response = await ac.get(f"/users/{id}")
+
+    expected_user = {
+        "name": "test",
+        "email": "test",
+        "phone_number": "test",
+        "id": 1,
+        "posts": [],
+    }
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == expected_user
+
+
+@pytest.mark.anyio
+async def test_get_all_route():
     async with AsyncClient(app=app, base_url=settings.alternative_host_for_api) as ac:
         response = await ac.get("/users/")
 
@@ -28,20 +54,59 @@ async def test_users_route():
 @pytest.mark.anyio
 async def test_create_route():
     async with AsyncClient(app=app, base_url=settings.alternative_host_for_api) as ac:
-        data = {
-            "name": "something",
-            "email": "something@gmail.com",
-            "phone_number": "1111111",
-            "password": "something",
-        }
-        response = await ac.post(url="/users/create/", json=data)
-        
-        login_user = await ac.post(
-            "/login", data={"username": "something@gmail.com", "password": "something"}
+        create_user_response = await ac.post(url="/users/create/", json=user_for_tests)
+        login_user_response = await ac.post(
+            "/login",
+            data={
+                "username": user_for_tests["email"],
+                "password": user_for_tests["password"],
+            },
         )
         await ac.delete(
-            f"/users/delete/{response.json()['id']}",
-            headers={"Authorization": f"Bearer {login_user.json()['access_token']}"},
+            f"/users/delete/{create_user_response.json()['id']}",
+            headers={"Authorization": f"Bearer {login_user_response.json()['access_token']}"},
         )
 
-    assert response.status_code == status.HTTP_201_CREATED
+    assert create_user_response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.anyio
+async def test_update_route():
+    async with AsyncClient(app=app, base_url=settings.alternative_host_for_api) as ac:
+        updated_info = {
+            "name": "something1",
+            "email": "something1@gmail.com",
+            "phone_number": "1111111",
+            "password": "something1",
+        }
+
+        create_user_response = await ac.post(url="/users/create/", json=user_for_tests)
+        login_user_response = await ac.post(
+            "/login",
+            data={
+                "username": user_for_tests["email"],
+                "password": user_for_tests["password"],
+            },
+        )
+        update_user_response = await ac.put(
+            f"/users/update/{create_user_response.json()['id']}",
+            headers={
+                "Authorization": f"Bearer {login_user_response.json()['access_token']}"
+            },
+            json=updated_info
+        )
+        get_user_by_id_response = await ac.get(
+            f"/users/{create_user_response.json()['id']}", 
+            headers={
+                "Authorization": f"Bearer {login_user_response.json()['access_token']}"
+            },
+        )
+        await ac.delete(
+            f"/users/delete/{create_user_response.json()['id']}",
+            headers={
+                "Authorization": f"Bearer {login_user_response.json()['access_token']}"
+            },
+        )
+
+    assert update_user_response.status_code == status.HTTP_200_OK
+    assert get_user_by_id_response.json()["email"] == updated_info["email"]
