1240a42694c2a56fbe9fe2764f9f13c5c5637f96
Dmytrenko-Roman <savitarreus@gmail.com>
2022-06-13 09:19:51 +0300

    ❄️ Test for post's /create route ❄️

--- backend/api/tests/test_post.py
+++ backend/api/tests/test_post.py
@@ -31,3 +31,41 @@ async def test_get_by_id_route():
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == expected_post
+
+
+@pytest.mark.anyio
+async def test_get_all_route():
+    async with AsyncClient(app=app, base_url=settings.alternative_host_for_api) as ac:
+        response = await ac.get("/posts/")
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.anyio
+async def test_create_route():
+    async with AsyncClient(app=app, base_url=settings.alternative_host_for_api) as ac:
+        create_post_response = await ac.post(
+            url="/posts/create/",
+            json=post_for_tests,
+            headers={
+                "Authorization": f"Bearer {access_token}"
+            },
+        )
+        await ac.delete(
+            f"/posts/delete/{create_post_response.json()['id']}",
+            headers={
+                "Authorization": f"Bearer {access_token}"
+            },
+        )
+
+        expected_post = {
+            "full_name": "something",
+            "last_place": "something",
+            "description": "something",
+            "image": "something",
+            "owner_id": 1,
+            "id": 2,
+        }
+
+    assert create_post_response.status_code == status.HTTP_201_CREATED
+    assert create_post_response.json() == expected_post
